e3f1735f02099d468a82da49eaa3a4eda6515cba
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-09 23:48:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,11 +16,11 @@ class Patient
     Appointment.new(date, self, doctor)
   end
 
-  def songs
-    Song.all.select{|song| song.genre == self}
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient == self}
   end
 
-  def artists
-    songs.map{|song| song.artist}
+  def doctors
+    appointments.map{|appointment| appointment.doctor}
   end
 end
