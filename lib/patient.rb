762a1f0d2431a91a5feddc7eade0aabd9b28478c
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-09 23:46:35 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,26 @@
+class Patient
+
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor)
+    
+  end 
+
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists
+    songs.map{|song| song.artist}
+  end
+end
