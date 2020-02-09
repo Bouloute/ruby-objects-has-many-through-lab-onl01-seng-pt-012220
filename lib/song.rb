4283f6668b471269c3ad77b32008f8d5142a14c7
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-09 23:38:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,10 +4,10 @@ class Song
 
   def initialize(name, artist, genre)
     @name = name
-    @artist = artist 
-    @genre = genre 
-  end 
-  
+    @artist = artist
+    @genre = genre
+  end
+
   def self.all
     @@all
   end
